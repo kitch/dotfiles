81d8f20277b75f9dc67f97bed2fb55245d8cdf31
Jake Kitchener <jakek@us.ibm.com>
2019-02-28 19:13:21 +0000

    switch to kitch dotfiles

--- workstation/entrypoint.sh
+++ workstation/entrypoint.sh
@@ -7,11 +7,11 @@ if [ ! -d ~/code/dotfiles ]; then
   # the reason we dont't copy the files individually is, to easily push changes
   # if needed
   cd ~/code
-  git clone --recursive https://github.com/fatih/dotfiles.git
+  git clone --recursive https://github.com/kitch/dotfiles.git
 fi
 
 cd ~/code/dotfiles 
-git remote set-url origin git@github.com:fatih/dotfiles.git
+git remote set-url origin git@github.com:kitch/dotfiles.git
 
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/zshrc ~/.zshrc

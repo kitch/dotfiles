735c75a4851d0b5ed79259ebbc08a3e591f443f1
Jake Kitchener <jakekit@gmail.com>
2019-02-28 10:40:34 -0500

    rename to devbox, switch to my image

--- workstation/bootstrap.sh
+++ workstation/bootstrap.sh
@@ -13,25 +13,25 @@ apt-get install -qq -y \
 echo "Creating directories"
 mkdir -p /mnt/code /mnt/secrets
 
-echo "=> Setting up dev service"
-cat > dockerdev.service <<EOF
+echo "=> Setting up devbox service"
+cat > dockerdevbox.service <<EOF
 [Unit]
-Description=dockerdev
+Description=dockerdevbox
 Requires=docker.service
 After=docker.service
 
 [Service]
 TimeoutStartSec=0
-ExecStartPre=-/usr/bin/docker kill dev
-ExecStartPre=-/usr/bin/docker rm dev
-ExecStartPre=/usr/bin/docker pull fatih/dev:89e3545
-ExecStart=/usr/bin/docker run -h dev -e TZ=Europe/Istanbul --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev fatih/dev:89e3545
+ExecStartPre=-/usr/bin/docker kill devbox
+ExecStartPre=-/usr/bin/docker rm devbox
+ExecStartPre=/usr/bin/docker pull kitch/devbox:latest
+ExecStart=/usr/bin/docker run -h dev -e TZ=UTC --net=host --rm -v /var/run/docker.sock:/var/run/docker.sock -v /mnt/code:/root/code -v /mnt/secrets:/root/secrets --cap-add=SYS_PTRACE --security-opt seccomp=unconfined --privileged --name dev kitch/devbox:latest
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-sudo mv dockerdev.service /etc/systemd/system/
+sudo mv dockerdevbox.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable dockerdev
 sudo systemctl start dockerdev

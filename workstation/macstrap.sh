5e74cb20300a7cf3968cfce7cb4f450eccf86447
kitch <jakek@us.ibm.com>
2019-03-04 10:58:56 -0500

    dynamic hostname

--- workstation/macstrap.sh
+++ workstation/macstrap.sh
@@ -1,4 +1,4 @@
-docker run -d -h dev -e TZ=UTC -p 3222:3222 -p 60000:60000/udp -p 60001:60001/udp -p 60002:60002/udp -p 60003:60003/udp -p 60004:60004/udp -p 60005:60005/udp -p 60006:60006/udp -p 60007:60007/udp -p 60008:60008/udp -p 60009:60009/udp -p 60010:60010/udp --rm \
+docker run -d -h `hostname -s` -e TZ=UTC -p 3222:3222 -p 60000:60000/udp -p 60001:60001/udp -p 60002:60002/udp -p 60003:60003/udp -p 60004:60004/udp -p 60005:60005/udp -p 60006:60006/udp -p 60007:60007/udp -p 60008:60008/udp -p 60009:60009/udp -p 60010:60010/udp --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v ~/devbox/code:/root/code \
   -v ~/devbox/secrets:/root/secrets \
